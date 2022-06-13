5828f29ea31ae9b2eb469e7df6b814945f5a4598
melanie <lovelystasik@gmail.com>
2022-06-13 18:39:00 +0300

    finished partners' page

--- src/app/partners/partners.component.ts
+++ src/app/partners/partners.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Partner } from './partner';
 
 @Component({
   selector: 'app-partners',
   templateUrl: './partners.component.html',
-  styleUrls: ['./partners.component.scss']
+  styleUrls: ['./partners.component.scss'],
 })
-export class PartnersComponent implements OnInit {
+export class PartnersComponent {
+  header = 'Наши партнёры';
 
-  constructor() { }
+  partners: Partner[] = [
+    { id: 1, url: '../../assets/svg/1.svg' },
+    { id: 2, url: '../../assets/svg/2.svg' },
+    { id: 3, url: '../../assets/svg/3.svg' },
+    { id: 4, url: '../../assets/svg/4.svg' },
+    { id: 5, url: '../../assets/svg/5.svg' },
+    { id: 6, url: '../../assets/svg/6.svg' },
+    { id: 7, url: '../../assets/svg/7.svg' },
+  ];
 
-  ngOnInit(): void {
-  }
-
+  constructor() {}
 }
