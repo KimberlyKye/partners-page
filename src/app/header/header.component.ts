9ab3b61f8a2cf7353f3d0cf7bbb683f117ebf361
melanie <lovelystasik@gmail.com>
2022-06-13 20:52:32 +0300

    create header for tablet and desktop

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../const/user';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  user: User = {
+    id: 0,
+    name: 'Константин',
+    picUrl: '../../assets/svg/userpic.svg',
+  };
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/partners/partner.ts
+++
@@ -1,4 +0,0 @@
-export interface Partner {
-  id: number;
-  url: string;
-}
